dc80204af0c83523e76dbf773de3ebf827a30366
Ben Glick <glick@glick.cloud>
2024-12-09 15:04:33 -0800

    import os

--- julia2/align.py
+++ julia2/align.py
@@ -26,8 +26,6 @@ def run_alignment(reads_sample_id, index_id, system_config, project_config):
                                                          cpus=True,
                                                          align_index="ALIGN")
 
-    logging.debug(lane, f"{project_config.project_dir}/raw_reads/lane{lane}-s{reads_sample_id}*R1*")
-
     #print(f"{combined_files_dir}/lane{lane}-s{index_id}*R1*")
     dir_1_filename = glob(
         f"{project_config.project_dir}/raw_reads/lane{lane}-s{reads_sample_id}*R1*")[0]
@@ -56,7 +54,6 @@ def run_all_samples(system_config, project_config, sequence_name_list):
         # For each sample
         for i in range(1, project_config.num_samples + 1):
             reads_sample_id = str(i).zfill(3)
-            print(reads_sample_id, index_id)
             run_alignment(reads_sample_id, index_id, system_config,
                           project_config)
 
@@ -68,7 +65,6 @@ def run_all_true_auto_samples(system_config, project_config, sequence_name_list)
         for i in range(1, project_config.num_samples + 1):
             reads_sample_id = str(i).zfill(3)
             if reads_sample_id in index_id:
-                print(reads_sample_id, index_id)
                 run_alignment(reads_sample_id, index_id, system_config,
                               project_config)
 
@@ -80,7 +76,6 @@ def run_all_allo_samples(system_config, project_config, sequence_name_list):
         for i in range(1, project_config.num_samples + 1):
             reads_sample_id = str(i).zfill(3)
             if int(reads_sample_id) not in int(index_id):
-                print(reads_sample_id, index_id)
                 run_alignment(reads_sample_id, index_id, system_config,
                               project_config)
 
@@ -98,7 +93,6 @@ def run_all_taxon_auto_samples(system_config, project_config, sequence_name_list
             if "c49446" in index_sample_id:
                 index_sample_id = "s018"
             if project_config.index_to_taxon_short[int(index_sample_id)] == project_config.index_to_taxon_short[int(reads_sample_id)]:
-                print(reads_sample_id, index_id)
                 run_alignment(reads_sample_id, index_id, system_config,
                               project_config)
 
@@ -158,6 +152,5 @@ def run_all_cross_lane_samples(system_config, project_config, sequence_name_list
 
 
             if index_lane != reads_lane:
-                print(reads_sample_id, index_id)
                 run_alignment(reads_sample_id, index_id, system_config,
                               project_config)
