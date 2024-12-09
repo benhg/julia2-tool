3fa92b939c019c82e967ea95c21265c93ab5ec18
Ben Glick <glick@glick.cloud>
2024-12-08 23:14:53 -0800

    setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,39 @@
-from setuptools import setup, find_packages
+"""
+@file setup.py
+@brief the PyPI required setup.py
+"""
 
-setup(
-    name="julia2",
-    version="0.1.0",
-    description="A command-line tool for detecting index hopping in RNA sequence data",
-    author="Benjamin Glick",
-    author_email="glick@glick.cloud",  # Replace with your email
-    url="https://github.com/benhg/julia2-tool",
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=[
-        "biopython"
-    ],
-    entry_points={
-        "console_scripts": [
-            "julia2=julia2:main",  # Maps "julia2" executable to the "main" function in julia2.py
-        ]
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.6",
+import os
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+version = "0.1.0"
+
+with open('requirements.txt') as f:
+    install_requires = f.readlines()
+
+setup(name='julia2-tool',
+      version=version,
+      license='MIT',
+      author="Benjamin Glick",
+      author_email='glick@glick.cloud',
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "Intended Audience :: Science/Research",
+          "License :: OSI Approved :: Apache Software License",
+          "Natural Language :: English", "Operating System :: OS Independent",
+          "Programming Language :: Python :: 3",
+          "Topic :: Scientific/Engineering"
+      ],
+      python_requires=">=3.6.0",
+      packages=find_packages(),
+      url='https://github.com/benhg/julia2-tool',
+      keywords=["RNA", "Sequencing", "Index Hopping", "Bioinformatics"],
+      entry_points={'console_scripts': ['julia2=julia2.julia2:main']},
+      install_requires=install_requires,
+      long_description=long_description,
+      long_description_content_type='text/markdown'
 )
