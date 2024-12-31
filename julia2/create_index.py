7699d4ba0151da1175351910a7a53073fdbbe694
Ben Glick <glick@glick.cloud>
2024-12-30 21:06:24 -0800

    test

--- julia2/create_index.py
+++ julia2/create_index.py
@@ -110,7 +110,7 @@ def find_reads(file, read_id, out_file, project_config):
         with open(reads_file, "r") as reads_handle:
             raw_reads = SeqIO.parse(reads_handle, "fasta")
             for record in raw_reads:
-                if sequence.strip() in record.id:
+                if sequence.strip() in f"{read_id}_{record.id}":
                     header = record.id
                     sequence_count += 1
                     if read_id not in header.split(" ")[0]:
