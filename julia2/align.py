d9c07bb99b83def79f415125b085622d14ebefd8
Ben Glick <glick@glick.cloud>
2024-12-30 23:50:26 -0800

    debug

--- julia2/align.py
+++ julia2/align.py
@@ -63,9 +63,9 @@ def run_all_true_auto_samples(system_config, project_config, sequence_name_list)
         for i in range(1, project_config.num_samples + 1):
             reads_sample_id = str(i).zfill(3)
             if f"s{reads_sample_id}_" in index_id:
-                #run_alignment(reads_sample_id, index_id, system_config,
-                #             project_config)
-                print(f"Running Job for {reads_sample_id}, {index_id}")
+                run_alignment(reads_sample_id, index_id, system_config,
+                              project_config)
+                logging.debug(f"Running Job for {reads_sample_id}, {index_id}")
 
 
 def run_all_allo_samples(system_config, project_config, sequence_name_list):
@@ -96,9 +96,9 @@ def run_all_taxon_auto_samples(system_config, project_config, sequence_name_list
             if "c49446" in index_sample_id:
                 index_sample_id = "s018"
             if project_config.sample_to_taxon_short[index_sample_id] == project_config.sample_to_taxon_short[f"s{reads_sample_id}"]:
-                #run_alignment(reads_sample_id, index_id, system_config,
-                #              project_config)
-                print(f"Running Job for {reads_sample_id}, {index_sample_id}, {project_config.sample_to_taxon_short[index_sample_id]}, {project_config.sample_to_taxon_short['s'+str(reads_sample_id)] }")
+                run_alignment(reads_sample_id, index_id, system_config,
+                              project_config)
+                logging.debug(f"Running Job for {reads_sample_id}, {index_sample_id}, {project_config.sample_to_taxon_short[index_sample_id]}, {project_config.sample_to_taxon_short['s'+str(reads_sample_id)] }")
 
 def run_all_intra_lane_samples(system_config, project_config, sequence_name_list):
 
