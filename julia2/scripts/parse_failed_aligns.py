dce5bba6b66b972b5d802905032959a2402d3601
Ben Glick <glick@glick.cloud>
2025-01-07 18:53:29 -0800

    another script

---
+++ julia2/scripts/parse_failed_aligns.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import glob
+import json
+
+project_dir = "/home/labs/binford/index_hopping_project/output/alignment_database_data/"
+failed = "/home/glick/julia2_tool_inputs/failed_aligns.txt"
+
+failed_details = {}
+
+with open(failed) as fh:
+	failed_files = fh.readlines()
+	for file in failed_files:
+		data = open(file).readlines()
+		index = data[0].split(" ")[0].split("index_")[1]
+		if failed_details.get(index) is None:
+			failed_details[index] = 1
+		else:
+			failed_details[index] += 1
+
+print(json.dumps(failed_details, indent=2))
+
